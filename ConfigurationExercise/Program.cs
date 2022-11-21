a5ea65833e711e288f1083124b8a20c60321cb30
darya dle <darya_2592@yahoo.com>
2022-11-21 06:21:17 -0800

    Add project files.

---
+++ ConfigurationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfigurationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("C:\\Users\\gh\\source\\repos\\ConfigurationExercise\\ConfigurationExercise\\jsconfig1.json").Build();
+            Setting setting = config.GetRequiredSection("setting").Get<Setting>();
+            Console.WriteLine("UserName: "+setting.UserName);
+            Console.WriteLine("Password: " + setting.Password);
+            Console.ReadKey();
+        }
+    }
+    public class Setting
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+    }
+}
